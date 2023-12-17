b06da3035445ec9f4a43fac710b78c3fc2bf7290
Gowri <gowri@ZipcodersMBP64.local>
2023-12-16 21:03:06 -0500

    wrote tests for few collection types

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,7 +1,12 @@
 package rocks.zipcode;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
-import java.util.Stack;
+
+import java.util.*;
 
 public class TestStack {
 
@@ -21,4 +26,91 @@ public class TestStack {
     }
 
     // Make a bigger test exercising more Stack methods.....
+
+    @Test
+    public void TestStack2(){
+        String expected = "Hello world";
+        Stack<String> stack = new Stack<>();
+        stack.push(expected);
+        String actual = stack.peek();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestStack3(){
+        String expected = "Hello world";
+        Stack<String> stack = new Stack<>();
+        stack.push(expected);
+        String actual = stack.pop();
+        assertEquals(expected, actual);
+    }
+        //HashSet
+    @Test
+    public void TestSet1() {
+        Set set = new HashSet<String>();
+        set.add("Hello World");
+        assertEquals(false, set.isEmpty());
+    }
+
+    @Test
+    public void TestSet2() {
+        String expected = "Hello World";
+        Set set = new HashSet<String>();
+        set.add(expected);
+        assertTrue(set.contains(expected));
+    }
+
+    @Test
+    public void TestSet3(){
+        String expected = "Hello World";
+        Set set = new HashSet<String >();
+        set.add(expected);
+        assertEquals(false, set.add(expected));
+    }
+
+    //List
+    @Test
+    public void TestList1() {
+        List list = new ArrayList<String>();
+        list.add("Hello World");
+        assertEquals(false, list.isEmpty());
+    }
+
+    @Test
+    public void TestList2(){
+        String expected = "Hello World";
+        List list = new ArrayList<String>();
+        list.add(expected);
+        assertEquals(expected, list.get(0));
+    }
+
+    //Map
+    @Test
+    public void TestMap(){
+        Map map = new HashMap<String, Person>();
+        String name = "Bob";
+        Person person =new Person(name, 10);
+        map.put(name, person);
+        assertTrue(map.containsValue(person));
+
+    }
+
+    @Test
+    public void TestMap1(){
+        Map map = new HashMap<String , Person>();
+        String name = "Bob";
+        Person person= new Person(name, 10);
+        map.put(name, person);
+        assertTrue(map.containsKey(name));
+    }
+    @Test
+    public void TestMap3 (){
+        Map map = new HashMap<String, Person>();
+        String name = "Bob";
+        Person person = new Person(name, 10);
+        map.put(name, person);
+        assertEquals(person, map.get(name));
+    }
+
+
 }
